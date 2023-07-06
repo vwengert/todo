9b8844a27754bca302440551747929deee8a51e0
Volker Wengert <vwengert@users.noreply.github.com>
2023-07-06 08:06:40 +0200

    adding a dialog to get todo text to add

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,15 +1,15 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
-#include <QDebug>
+#include <QInputDialog>
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui{new Ui::MainWindow}
-    , mTasks{}
+MainWindow::MainWindow( QWidget* parent )
+    : QMainWindow( parent )
+    , ui( new Ui::MainWindow )
+    , mTasks()
 {
-    ui->setupUi(this);
-    connect(ui->addTaskButton, &QPushButton::clicked, this, &MainWindow::addTask);
+    ui->setupUi( this );
+    connect( ui->addTaskButton, &QPushButton::clicked, this, &MainWindow::addTask );
 }
 
 MainWindow::~MainWindow()
@@ -19,10 +19,19 @@ MainWindow::~MainWindow()
 
 void MainWindow::addTask()
 {
-    qDebug() << "User clicked on add task button!";
-    Task* task = new Task("Untitled Task");
-    mTasks.append(task);
-    ui->tasksLayout->addWidget(task);
+    bool ok;
+    QString name =
+        QInputDialog::getText( this,
+                               tr( "Add task" ),
+                               tr( "Task name" ),
+                               QLineEdit::Normal,
+                               tr( "Untitled task" ), &ok );
+
+    if( ok && !name.isEmpty() ) {
+        Task* task = new Task( name );
+        mTasks.append( task );
+        ui->tasksLayout->addWidget( task );
+    }
 }
 
 #include "moc_mainwindow.cpp"
